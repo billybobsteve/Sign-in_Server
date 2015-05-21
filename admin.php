d9f1d87ddff1b737b5a328ba91d156eb48296f55
Idoncar <nathanspring0@gmail.com>
2015-05-21 11:55:04 -0400

    Added admin.php + admin_style.css

---
+++ admin.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>ADMIN</title>
+	</head>
+
+	<body>
+		
+		<input type = "button" id = "submit">Submit</input>
+
+	</body>
+	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
+	
+</html>
