fd808ef4109ca91ef11961f2e860e8e9c0a822ed
Idoncar <nathanspring0@gmail.com>
2015-05-21 11:59:40 -0400

    testing apache update

--- admin.php
+++ admin.php
@@ -4,8 +4,11 @@
 	</head>
 
 	<body>
+
+		<p> HI THERE </p>
 		
 		<input type = "button" id = "submit">Submit</input>
+
 
 	</body>
 	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
