3ace62e8add9e947979d8494d3869e33ed723128
Thomas <-->
2015-05-21 14:29:42 -0400

    Syntax error

--- action_out.php
+++ action_out.php
@@ -5,28 +5,21 @@ $time_out = date('Y-m-d H:i:s');
 echo "{$name} has just been signed out to {$dest}"; 
 
 $dbhost = 'localhost';
-$dbhost = 'root';
+$dbuser = 'root';
 $dbpass = 'ec2inmybutt';
-//$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-
-$conn = mysql_connect('localhost', 'root', 'ec2inmybutt');
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
 
 if (!conn) {
-	/*die('Could not connect: ' . mysql_error());*/
-	echo ('Could not connect: ' . mysql_error());
+	die('Could not connect: ' . mysql_error());
 }
 
-//$sql = 'INSERT INTO Students (Name, Location, TimeOut) VALUES ({$name}, {$dest}, {$time_out})';
-
-$sql = 'INSERT INTO Students (Name, Location) VALUES ("Chase", "Chipotle")';
-
+$sql = 'INSERT INTO Students (Name, Location, TimeOut) VALUES ({$name}, {$dest}, {$time_out})';
 
 mysql_select_db('signoutdb');
 
 $retval = mysql_query($sql, $conn);
 if(! $retval ) {
-  /*die('Could not enter data: ' . mysql_error());*/
-  echo ('Could not enter data: ' . mysql_error());
+  die('Could not enter data: ' . mysql_error());
 }
 
 mysql_close($conn);
