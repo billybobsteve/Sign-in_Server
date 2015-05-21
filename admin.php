4e364dee7819f368cf63815dbcce3b79f9344109
Idoncar <nathanspring0@gmail.com>
2015-05-21 14:09:29 -0400

    admin.php test4

--- admin.php
+++ admin.php
@@ -7,14 +7,18 @@
 
 		<h3> Choose Action </h3>
 
-		<form action = "admin.php" method = "post">
+		<form action = "admin.php" method = "post" name = "list">
 		<select required id = "options" onChange = "down_change()">
 			<option value = "out_students">Get Out Students</option>
 			<option value = "all_students">Get All Students</option>
 			<option value = "edit_student">Edit Student</option>  	
 		</select>
-
-		<input type = "text" id = "student" value = "Enter Student Full Name">
+		<div id = "other_info" style = "display: none;">
+		<input type = "text" placeholder = "Enter Student Full Name">
+		<input type = "text" placeholder = "Enter Student Location">
+		<input type = "text" placeholder = "Enter Student Time in">
+		<input type = "text" placeholder = "Enter Student Time Out">
+	</div>
 		<input type = "submit" value = "Submit">
 
 
@@ -22,6 +26,6 @@
 
 	</body>
 	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
-	<script src="jquery-2.1.4.min.js">
-	<script src="script.js">
+	<script src="jquery-2.1.4.min.js"/>
+	<script src="script.js"/> 
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+$('list').on('change', function(){
+	if($(this).val() === "edit_student"){
+		$("#other_info").show();
+	}
+	else{
+		$("#other_info").hide();
+	}
+});
