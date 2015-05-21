162ca1c2491699a55995952e320f691f9f0ddb9e
Idoncar <nathanspring0@gmail.com>
2015-05-21 13:17:44 -0400

    admin.php test1

--- admin.php
+++ admin.php
@@ -5,10 +5,16 @@
 
 	<body>
 
-		<p> HI THERE </p>
-		
-		<input type = "button" id = "submit">Submit</input>
+		<h3> Choose Action </h3>
 
+		<form action = "admin.php" method = "post">
+		<select required>
+			<option value = "out_students">Get Out Students</option>
+			<option value = "all_students">Get All Students</option>
+			<option value = "edit_student">Edit Student</option>  
+		</select>
+		<input type = "submit" value = "Submit">
+	</form>
 
 	</body>
 	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
