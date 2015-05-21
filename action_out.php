0f275e0d4557ffa05651ffa96037e04f6c5d36f7
Thomas <-->
2015-05-21 13:49:40 -0400

    mysql test

--- action_out.php
+++ action_out.php
@@ -1,5 +1,24 @@
 <?php 
 $name = htmlspecialchars($_POST['name']);
 $dest = htmlspecialchars($_POST['destination']);
+$time_out = date('Y-m-d H:i:s');
 echo "{$name} has just been signed out to {$dest}"; 
+
+$dbhost = "52.7.134.39"
+$dbhost = 'root';
+$dbpass = 'ec2inmybutt';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+if (!conn) {
+	die('Could not connect: ' . mysql_error());
+}
+
+$sql = 'INSERT INTO Students (Name, Location, TimeOut) VALUES ({$name}, {$dest}, {$time_out})';
+
+mysql_select_db('signoutdb');
+
+$retval = mysql_query($sql, $conn);
+
+mysql_close($conn);
+
 ?>
