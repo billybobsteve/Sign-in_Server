e91c515c8b5f8cbccaa613993ada40ac2e9de608
Idoncar <nathanspring0@gmail.com>
2015-05-21 14:00:48 -0400

    admin.php test3 + jquery

--- admin.php
+++ admin.php
@@ -3,19 +3,6 @@
 		<title>ADMIN</title>
 	</head>
 
-	<script type = "text/javascript">
-		function down_change(){
-			var selected = document.getElementById("options");
-			var sv = selected.options[sel.selectedIndex].value;
-			var text = document.getElementById("student");
-			if(sv === "edit_student")
-				text.disabled = false;
-			else
-				text.disabled = true;
-		}
-
-	</script>
-
 	<body>
 
 		<h3> Choose Action </h3>
@@ -26,6 +13,7 @@
 			<option value = "all_students">Get All Students</option>
 			<option value = "edit_student">Edit Student</option>  	
 		</select>
+
 		<input type = "text" id = "student" value = "Enter Student Full Name">
 		<input type = "submit" value = "Submit">
 
@@ -33,5 +21,7 @@
 	</form>
 
 	</body>
-	<link rel = "stylesheet" type = "text/css" href = "admin_style.css"
+	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
+	<script src="jquery-2.1.4.min.js">
+	<script src="script.js">
 </html>
