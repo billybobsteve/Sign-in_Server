e7fb06ca2aeea7888ac96a456008c8e55e763a72
Idoncar <nathanspring0@gmail.com>
2015-05-21 13:40:18 -0400

    admin.php test2

--- admin.php
+++ admin.php
@@ -3,20 +3,35 @@
 		<title>ADMIN</title>
 	</head>
 
+	<script type = "text/javascript">
+		function down_change(){
+			var selected = document.getElementById("options");
+			var sv = selected.options[sel.selectedIndex].value;
+			var text = document.getElementById("student");
+			if(sv === "edit_student")
+				text.disabled = false;
+			else
+				text.disabled = true;
+		}
+
+	</script>
+
 	<body>
 
 		<h3> Choose Action </h3>
 
 		<form action = "admin.php" method = "post">
-		<select required>
+		<select required id = "options" onChange = "down_change()">
 			<option value = "out_students">Get Out Students</option>
 			<option value = "all_students">Get All Students</option>
-			<option value = "edit_student">Edit Student</option>  
+			<option value = "edit_student">Edit Student</option>  	
 		</select>
+		<input type = "text" id = "student" value = "Enter Student Full Name">
 		<input type = "submit" value = "Submit">
+
+
 	</form>
 
 	</body>
-	<link rel = "stylesheet" type = "text/css" href = "admin_style.css">
-	
+	<link rel = "stylesheet" type = "text/css" href = "admin_style.css"
 </html>
