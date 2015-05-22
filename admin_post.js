6efaf19c96215826b2a634a02fc7505f5cfb9f8f
Thomas <-->
2015-05-22 10:55:39 -0400

    ajax async test

---
+++ admin_post.js
@@ -0,0 +1,13 @@
+post() {
+	//var option = document.getElementById("options").value;
+	var form_data = ('#selection_form').serialize();
+	$.ajax({
+		url:'action_admin.php',
+		type:'post',
+		data:form_data
+		done:function(data, textStatus, jqXHR){
+			alert("success");
+			console.log(data);
+		}
+	});
+}
