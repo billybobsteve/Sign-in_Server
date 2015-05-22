679e76bd8017bed7738dee68f67b5c195dc66618
Idoncar <nathanspring0@gmail.com>
2015-05-22 15:11:54 -0400

    more test

--- admin.php
+++ admin.php
@@ -47,9 +47,16 @@
 	</script> 
 	<script>
 		$(function(){
-			var availableTags = ["dicks", "penis", "penisbreath", "Poopy"];
-			$("search").autocomplete({
-				source: availableTags
+				var countries = [
+	   			{ value: 'Andorra', data: 'AD' },
+	   			{ value: 'Zimbabwe', data: 'ZZ' }
+				];
+
+			$('#search').autocomplete({
+	    		lookup: countries,
+	    		onSelect: function (suggestion) {
+	        		alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+	    		}
 			});
 		});
 	</script>
