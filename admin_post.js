49d9e2009adb00cedc9a7595f9fceeb261dde1e7
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-22 11:14:18 -0400

    =

--- admin_post.js
+++ admin_post.js
@@ -1,7 +1,7 @@
 function post() {
 	//var option = document.getElementById("options").value;
 	var form_data = ('#selection_form').serialize();
-	var ajax_post $.ajax({
+	var ajax_post = $.ajax({
 		url:'action_admin.php',
 		type:'post',
 		data:form_data
