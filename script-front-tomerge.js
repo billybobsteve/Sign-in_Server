ff80fb444c2a0be005d4df55f2add71292e4cfa1
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-26 10:47:12 -0400

    works?.js

--- script-front-tomerge.js
+++ script-front-tomerge.js
@@ -6,7 +6,7 @@ var insert_overlay = function() {
 		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
 		overlay.style.zIndex = 10;
 	}
-	else if (zIndex < 0) {
+	else if (zIndex > 0) {
 		overlay.style.backgroundColor = "rgba(0,0,0,0)";
 		overlay.style.zIndex = -10;
 	}
