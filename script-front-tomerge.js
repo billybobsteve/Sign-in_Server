27305435b53e906095e5594c6c9fc93a137db7ae
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-26 10:36:00 -0400

    C-S-S

--- script-front-tomerge.js
+++ script-front-tomerge.js
@@ -1,3 +1,5 @@
 var insert_overlay = function() {
-	overlay = document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0.5)";
+	overlay = document.getElementById("overlay");
+	overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+	overlay.style.zIndex = 10;
 }
