eda3617debdcbab7081ed17dcb1402149c4886e2
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-26 10:57:04 -0400

    css update

--- script-front-tomerge.js
+++ script-front-tomerge.js
@@ -1,13 +1,16 @@
 var insert_overlay = function() {
 	overlay = document.getElementById("overlay");
+	confirmation = document.getElementById("confirmation");
 	zIndex = window.getComputedStyle(overlay).getPropertyValue('z-index');
 	//zIndex = style.getPropertyValue('z-index');
 	if (zIndex < 0) {
 		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+		confirmation.style.backgroundColor = "rgba(0,0,0,.5)";
 		overlay.style.zIndex = 10;
 	}
 	else if (zIndex > 0) {
 		overlay.style.backgroundColor = "rgba(0,0,0,0)";
+		confirmation.style.backgroundColor = "rgba(0,0,0,0)";
 		overlay.style.zIndex = -10;
 	}
 }
