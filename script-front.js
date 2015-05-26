a4dd3a9d3ba50e338271329a4a4df0f9a78df582
Chase Goddard <chase@chasegoddard.com>
2015-05-26 13:49:51 -0400

    test

--- script-front.js
+++ script-front.js
@@ -1,9 +1,10 @@
+var $ = jQuery;
 var pad = function(x){
 	if(x<10){
 		return "0"+x;
 	}
 	return x;
-}
+};
 var now = new Date();
 var hours = now.getHours();
 var mins = now.getMinutes();
@@ -18,8 +19,9 @@ var format = function (v) {
 	v = v - h*3600;
 	var m = pad(Math.floor(v / 60));
 	var ampm = (h < 12) ? "AM" : "PM";
-	if(h!=12)
+	if(h!==12){
 		h %= 12;
+	}
 	h = pad(h);
 	console.log(h + ":" + "m");
 	return h + ":" + m + " " + ampm;
@@ -29,29 +31,31 @@ $("#time").val(format(sec_since_midnight));
 
 $("#upbutton").click(function(){
 	sec_since_midnight+=60;
-	if(sec_since_midnight > 86400)
+	if(sec_since_midnight > 86400){
 		sec_since_midnight = 86400;
+		}
 	$("#time").val(format(sec_since_midnight));
-})
+});
 
 $("#downbutton").click(function(){
 	sec_since_midnight-=60;
-	if(sec_since_midnight < 0)
+	if(sec_since_midnight < 0){
 		sec_since_midnight = 0;
+		}
 	$("#time").val(format(sec_since_midnight));
-})
+});
 
 $("#plus").click(function(){
-	if($("#name").val() != "" ){
+	if($("#name").val() !== "" ){
 		$("#nameList").append(
 			"<li>" + $("#name").val() +"</li>"
 		);
-		$("#name").val("")
+		$("#name").val("");
 	}
 });
 
 $('body').on('keypress', '#name', function(args) {
-	if (args.keyCode == 13) {
+	if (args.keyCode === 13) {
 		$('#plus').click();
 		return false;
 	}
