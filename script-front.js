bf3173f88b90d94ac5df97105bac3558f1db3161
Chase Goddard <chase@chasegoddard.com>
2015-05-26 13:55:35 -0400

    many much lines

--- script-front.js
+++ script-front.js
@@ -1,76 +1,77 @@
-var $ = jQuery;
-var pad = function(x){
-	if(x<10){
-		return "0"+x;
-	}
-	return x;
-};
-var now = new Date();
-var hours = now.getHours();
-var mins = now.getMinutes();
-var sec_since_midnight = hours*3600+mins*60;
-console.log("CHASE IS THE BEST PERSON");
-
-
-var format = function (v) {
-	console.log(v);
-	v = Number(v);
-	var h = Math.floor(v / 3600);
-	v = v - h*3600;
-	var m = pad(Math.floor(v / 60));
-	var ampm = (h < 12) ? "AM" : "PM";
-	if(h!==12){
-		h %= 12;
-	}
-	h = pad(h);
-	console.log(h + ":" + "m");
-	return h + ":" + m + " " + ampm;
-};
-
-$("#time").val(format(sec_since_midnight));
-
-$("#upbutton").click(function(){
-	sec_since_midnight+=60;
-	if(sec_since_midnight > 86400){
-		sec_since_midnight = 86400;
+$(document).ready(function(){
+	var pad = function(x){
+		if(x<10){
+			return "0"+x;
 		}
-	$("#time").val(format(sec_since_midnight));
-});
+		return x;
+	};
+	var now = new Date();
+	var hours = now.getHours();
+	var mins = now.getMinutes();
+	var sec_since_midnight = hours*3600+mins*60;
+	console.log("CHASE IS THE BEST PERSON");
 
-$("#downbutton").click(function(){
-	sec_since_midnight-=60;
-	if(sec_since_midnight < 0){
-		sec_since_midnight = 0;
+
+	var format = function (v) {
+		console.log(v);
+		v = Number(v);
+		var h = Math.floor(v / 3600);
+		v = v - h*3600;
+		var m = pad(Math.floor(v / 60));
+		var ampm = (h < 12) ? "AM" : "PM";
+		if(h!==12){
+			h %= 12;
 		}
+		h = pad(h);
+		console.log(h + ":" + "m");
+		return h + ":" + m + " " + ampm;
+	};
+
 	$("#time").val(format(sec_since_midnight));
-});
 
-$("#plus").click(function(){
-	if($("#name").val() !== "" ){
-		$("#nameList").append(
-			"<li>" + $("#name").val() +"</li>"
-		);
-		$("#name").val("");
-	}
-});
+	$("#upbutton").click(function(){
+		sec_since_midnight+=60;
+		if(sec_since_midnight > 86400){
+			sec_since_midnight = 86400;
+			}
+		$("#time").val(format(sec_since_midnight));
+	});
 
-$('body').on('keypress', '#name', function(args) {
-	if (args.keyCode === 13) {
-		$('#plus').click();
-		return false;
-	}
-});
+	$("#downbutton").click(function(){
+		sec_since_midnight-=60;
+		if(sec_since_midnight < 0){
+			sec_since_midnight = 0;
+			}
+		$("#time").val(format(sec_since_midnight));
+	});
 
-$("in-tab").click(function(){
-	$("in-tab").addClass("activeLink");
-	$("out-tab").removeClass("activeLink");
-	$("destination").addClass("hide");
-	console.log("in clicked");
-});
+	$("#plus").click(function(){
+		if($("#name").val() !== "" ){
+			$("#nameList").append(
+				"<li>" + $("#name").val() +"</li>"
+			);
+			$("#name").val("");
+		}
+	});
 
-$("out-tab").click(function(){
-	$("out-tab").addClass("activeLink");
-	$("in-tab").removeClass("activeLink");
-	$("destination").removeClass("hide");
-	console.log("out clicked");
+	$('body').on('keypress', '#name', function(args) {
+		if (args.keyCode === 13) {
+			$('#plus').click();
+			return false;
+		}
+	});
+
+	$("in-tab").click(function(){
+		$("in-tab").addClass("activeLink");
+		$("out-tab").removeClass("activeLink");
+		$("destination").addClass("hide");
+		console.log("in clicked");
+	});
+
+	$("out-tab").click(function(){
+		$("out-tab").addClass("activeLink");
+		$("in-tab").removeClass("activeLink");
+		$("destination").removeClass("hide");
+		console.log("out clicked");
+	});
 });
