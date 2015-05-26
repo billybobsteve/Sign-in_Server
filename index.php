f4fb494778b4f2fc06e6c8c56d2c322f14cbda56
Idoncar <nathanspring0@gmail.com>
2015-05-26 10:47:09 -0400

    labelz

--- index.php
+++ index.php
@@ -44,10 +44,10 @@
 		<form id = "sign_out_form" name = "sign_out_form" action="action_out.php" method="post">
 
 			<div id = "dataInput">
-				Full name: <span> <input type="text" name = "name" id="name"/>
+				<label for = "name">Full Name: </label> <span> <input type="text" name = "name" id="name"/>
 					<a id="plus" href="#"> <strong> + </strong> <a> </span>
-				Destination: <input type="text" name = "destination" class = "in"/>
-				Time Out: <input type="text" class = "in" id="time"/>
+				<label for = "destination">Destination: </label> <input type="text" name = "destination" class = "in"/>
+				<label for = "time">Time Out: </label><input type="text" class = "in" id="time"/>
 				<p>
 					<div id = "groupList">
 						<span> Previously entered names: </span>
