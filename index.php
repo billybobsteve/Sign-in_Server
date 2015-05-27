3a1fc1264063680de31bb69af9b4e2a542cbcb81
RandmNewb <wuguishifu@me.com>
2015-05-27 13:12:14 -0400

    try again

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 						<input type="text" class = "in" id="time" autocomplete = "off"/>
 					</div>
 
-					<div>
+					<div id="arrows_sections">
 						<div id="upbutton" class="arrow">&#x25b2;</div>
 						<div id="downbutton" class="arrow">&#x25bc;</div>
 					</div>
