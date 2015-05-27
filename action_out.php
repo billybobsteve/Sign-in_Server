ea82c3da46cbd242e16b22ce0a61a1084f6edb06
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-27 18:32:46 -0400

    Fuck PHP debugging so much

--- action_out.php
+++ action_out.php
@@ -5,6 +5,7 @@ $time_out = htmlspecialchars($_POST['datetime']);
 $current_date = new DateTime();
 $time_out = $current_date->format('Y-m-d ') . $time_out;
 echo $time_out;
+echo gettype($time_out);
 die();
 $time_out = new DateTime($time_out);
 //$time_out = datetime::createfromformat('Y-m-d H:i:s', $time_out);
