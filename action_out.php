7204664c58e1b5391cbaa307fb6d6697e7b64f3b
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-27 18:24:25 -0400

    ............................................................

--- action_out.php
+++ action_out.php
@@ -3,10 +3,11 @@ $name = htmlspecialchars($_POST['name']);
 $dest = htmlspecialchars($_POST['destination']);
 $time_out = htmlspecialchars($_POST['datetime']);
 $time_out = date("Y-m-d ") . $time_out;
+
+$time_out = datetime::createfromformat('Y-m-d H:i:s',$time_out);
 echo $time_out;
 die();
 
-$time_out = datetime::createfromformat('Y-m-d H:i:s',$time_out);
 $server_time_out = date("Y-m-d H:i:s");
 echo $time_out;
 
