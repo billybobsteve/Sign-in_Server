cea0a29a68f433468a665db1c7efe8d7b00f29f5
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-27 18:19:34 -0400

    debugging

--- action_out.php
+++ action_out.php
@@ -5,6 +5,7 @@ $time_out = htmlspecialchars($_POST['datetime']);
 $time_out = date("Y-m-d ") . $time_out;
 $time_out = datetime::createfromformat('Y-m-d H:i:s',$time_out);
 $server_time_out = date("Y-m-d H:i:s");
+echo $time_out;
 
 $name_array = explode(',', $name);
 
