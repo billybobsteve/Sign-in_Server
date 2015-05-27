29852fbc4029824d2f613ac451d87da95d6103bf
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-27 10:39:24 -0400

    js stuff

--- action_out.php
+++ action_out.php
@@ -25,15 +25,17 @@ mysql_select_db('signoutdb');
 for ($i = 0; $i < count($name_array); $i++) {
 	$current_name = $name_array[$i];
 	$sql_name_check = "SELECT * FROM Students WHERE Name = '{$current_name}';";
-	echo mysql_query($sql_name_check, $conn);
-	break;
+	$name_check = mysql_query($sql_name_check, $conn);
+	if (mysql_num_rows($sql_name_check) > 0)
+		echo '-1';
 	$sql = "INSERT INTO Students (Name, Location, TimeOut) VALUES ('{$current_name}', '{$dest}', '{$time_out}'); ";
 	$retval = mysql_query($sql, $conn);
 
 	if(! $retval ) {
 	  die('Could not enter data: ' . mysql_error());
 	}
-	echo $current_name . ','; //"\r\n"; 
+	//echo $current_name . ','; //"\r\n"; 
+	echo '1' . ',';
 }
 
 mysql_close($conn);
--- async_post_index.js
+++ async_post_index.js
@@ -1,3 +1,4 @@
+var global = "";
 function post() {
 	//var option = document.getElementById("options").value; //if (location.pathname.includes("index.php")) {
 	if (document.getElementById("destination").value.trim() === "") {
@@ -6,6 +7,7 @@ function post() {
 	}
 	var interim = $('#sign_out_form').serializeArray();
 	var form_data = "name=" + serialize_string(interim[0].value); // + ",";
+	//global += interim[0].value;
 
 	//console.log(form_data);
 	//console.log("test");
@@ -16,12 +18,18 @@ function post() {
 		//name_list[i] = list[i].innerText;
 		if (form_data !== "name=") {
 			form_data += ",";
+			//global += ",";
 		}
 		form_data += serialize_string(list[i].innerText);
 	}
 
+	global = form_data;
+
 	form_data += "&destination=" + serialize_string(interim[1].value);
 
+	form_data = form_data.toLowerCase().trim();
+
+	console.log(global);
 	console.log(form_data);
 	//console.log("test2");
 
@@ -44,19 +52,31 @@ function return_data(data, textStatus, jqXHR) {
 	var signed_out = data.trim().split(',');
 	signed_out.pop();
 	var message = "";
+	var message_error = "";
 	console.log(signed_out);
 	console.log(signed_out.length);
 	if (signed_out.length === 1) {
-		message = '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + signed_out[0] + '</span>' + " has successfully been signed out to " + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>';
+		if (signed_out[0] === 'T')
+			message = '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + signed_out[0] + '</span>' + " has successfully been signed out to " + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>';
+		else
+			message = '<span style="color:#FF0000;font-size:24px;opacity:.9;"> ERROR: ' + signed_out[0] + '</span>' + " has already been signed out.";
 		insert_overlay(message);
 		return;
 	}
 	for (var i = 0; i < signed_out.length; i++) {
-		message += signed_out[i] + ", ";
+		if (signed_out[0] === 'T') {
+			message += signed_out[i] + ", ";
+		}
+		else {
+			message_error += signed_out[i] + ", ";
+		}
 	}
 	//console.log(message);
 	//console.log(message.substring(0,message.length-2));
-	message = '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + message.substring(0, message.length-2) + '</span>' + ' have successfully been signed out to ' + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>';
+	message = (message_error.trim() === "") ? '<span style="color:#00FF00;font-size:24px;opacity:.9;">' +
+	message.substring(0, message.length-2) + '</span>' + ' have successfully been signed out to ' + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>' :'<span style="color:#00FF00;font-size:24px;opacity:.9;">' +
+	message.substring(0, message.length-2) + '</span>' + ' have successfully been signed out to ' + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span> <div style="color:#FF0000;font-size:24px;opacity:.9;"> ERROR: ' +
+	message_error.substring(0, message.length-2) + ' was already signed out. </div>';
 	insert_overlay(message);
 }
 
