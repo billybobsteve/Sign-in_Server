3ef73edbaecc79e0a6a96dcad98da55c463f4098
Chase Goddard <chase@chasegoddard.com>
2015-05-27 14:03:50 -0400

    okeh v2

--- admin/async_post_admin.js
+++ admin/async_post_admin.js
@@ -12,8 +12,16 @@ function post() {
 	ajax_post.done(return_data);
 }
 
+var edit = function(obj){
+	$("span", obj).html("<input type='text'>")
+}
+
 function return_data(data, textStatus, jqXHR) {
 	//if (location.pathname.includes("admin.php")) {
 		$("#table").html(data);
+		$(".table-entry").append("<a href='#' class='edit-button'>Edit</a>");
+		$("#edit-button").click(function(){
+			edit($(this).parent());
+		});
 	//}
 }
--- admin/script-admin.js
+++ admin/script-admin.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	var edit = function(obj){
-		$("span", obj).html("<input type='text'>")
-	}
-	/*
 	$('#options').on('change', function(){
 		if($(this).val() === "edit_student"){
 			$("#other_info").show();
@@ -10,15 +6,6 @@ $(document).ready(function(){
 		else{
 			$("#other_info").hide();
 		}
-	});
-*/
-	$("#submit").click(function(){
-		console.log("okeh");
-		console.log(document.getElementsByClassName("table-entry").length);
-		$(".table-entry").append("<a href='#' class='edit-button'>Edit</a>");
-	});
-	$("#edit-button").click(function(){
-		edit("#edit-button").parent();
-	});
+	});	
 });
 
