8e196ac9336bf6d5f3991b44f9032402c51cb91c
Chase Goddard <chase@chasegoddard.com>
2015-05-27 09:42:33 -0400

    classy

--- script-front.js
+++ script-front.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
 		var index = $("#nameList li").length + 1;
 		if($("#name").val() !== "" ){
 			$("#nameList").append(
-				"<li id='name-item-"+index+"'>" + $("#name").val() +
+				"<li class='name-item' id='name-item-"+index+"'>" + $("#name").val() +
 				"<a href='#' id='name-button-"+index+"'>-</a>" +
 				"</li>" 
 			);
