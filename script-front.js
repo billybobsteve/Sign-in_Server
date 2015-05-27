31e424f9a9b9f8eb2c184da444d11fe54d693c8b
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-26 22:16:17 -0400

    fix

--- script-front.js
+++ script-front.js
@@ -89,7 +89,7 @@ var insert_overlay = function(message) {
 	confirmation = document.getElementById("confirmation");
 	zIndex = window.getComputedStyle(overlay).getPropertyValue('z-index');
 	//zIndex = style.getPropertyValue('z-index');
-	if (zIndex <= 0) {
+	if (zIndex === "-11") {
 		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
 		confirmation.style.backgroundColor = "rgba(0,0,0,.5)";
 		overlay.style.zIndex = 10;
@@ -98,7 +98,7 @@ var insert_overlay = function(message) {
 		overlay.onclick = insert_overlay("");
 		//setTimeout(insert_overlay, 5000);
 	}
-	else if (zIndex > 0) {
+	else { //if (zIndex > 0) {
 		overlay.onclick = null;
 		overlay.style.backgroundColor = "rgba(0,0,0,0)";
 		confirmation.style.backgroundColor = "rgba(0,0,0,0)";
