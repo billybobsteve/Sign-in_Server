2d166640b6ba11faf582eb0ceecc1c9ad7b46a03
Chase Goddard <chase@chasegoddard.com>
2015-05-27 14:19:00 -0400

    +=
    thomas you ass

--- async_post_index.js
+++ async_post_index.js
@@ -7,7 +7,7 @@ function post() {
 	var time = document.getElementById('time').value;
 	var hour = parseInt(time.substring(0, 2));
 	var min = parseInt(time.substring(2, 4));
-	var hour += (time.substring(4, time.length) == 'AM') ? 0 : 12;
+	var hour = (time.substring(4, time.length) == 'AM') ? 0 : 12;
 
 
 	for (var i = 0; i < list.length; i++) {
