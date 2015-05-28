4bf2800be2aa56996c61468e49b8b6872661f273
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-28 11:19:34 -0400

    error generalities

--- async_post_index.js
+++ async_post_index.js
@@ -172,7 +172,7 @@ function return_data_out(data, textStatus, jqXHR) {
 	message.substring(0, message.length-2) + '</span>' + (signed_out_success > 1 ? ' have' : ' has') + ' successfully been signed out to ' + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>' :
 	"";
 	message += (signed_out_failure != 0) ? '<div style="color:#FF0000;font-size:24px;opacity:.9;"> Error: ' +
-	message_error.substring(0, message_error.length-2) + (signed_out_failure > 1 ? ' have' : ' has') + ' already been signed out. </div>' : "";
+	message_error.substring(0, message_error.length-2) + (signed_out_failure > 1 ? ' have' : ' has') + ' already been signed out or' + (signed_out_failure > 1 ? ' have' : ' has') + ' already signed back in. </div>' : "";
 
 	//message = (message_error.trim() === "") ? '<span style="color:#00FF00;font-size:24px;opacity:.9;">' +
 	//message.substring(0, message.length-2) + '</span>' + (signed_out_success > 1 ? ' have' : ' has') + ' successfully been signed out to ' + '<span style="color:#00FF00;font-size:24px;opacity:.9;">' + document.getElementById("destination").value.trim() + '</span>' : '<span style="color:#00FF00;font-size:24px;opacity:.9;">' +
