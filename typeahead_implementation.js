6bb6b03ab306182a0c1c7f3043612b2b4635be92
Chase Goddard <chase@chasegoddard.com>
2015-05-28 10:53:25 -0400

    f

--- typeahead_implementation.js
+++ typeahead_implementation.js
@@ -1,25 +1,25 @@
 var substringMatcher = function(strs) {
-	return function findMatches(q, cb) {
-		var matches, substringRegex;
-		 
-	    // an array that will be populated with substring matches
-	    matches = [];
-	 
-	    // regex used to determine if a string contains the substring `q`
-	    substrRegex = new RegExp(q, 'i');
-		 
-	    // iterate through the pool of strings and for any string that
-	    // contains the substring `q`, add it to the `matches` array
-	    $.each(strs, function(i, str) {
-	      if (substrRegex.test(str)) {
-	        matches.push(str);
-	      }
-	    });
-		 
-	    cb(matches);
-	};
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+ 
+    cb(matches);
+  };
 };
-	 
+ 
 var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
@@ -30,12 +30,13 @@ var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
 ];
+ 
 $('#the-basics .typeahead').typeahead({
-	hint: true,
-	highlight: true,
-	minLength: 1
+  hint: true,
+  highlight: true,
+  minLength: 1
 },
 {
-  	name: 'states',
-  	source: substringMatcher(states)
+  name: 'states',
+  source: substringMatcher(states)
 });
