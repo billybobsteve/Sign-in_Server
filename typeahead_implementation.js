a6ccd6e605eeb5bb9ae9bca0dfdf28a62212476e
Chase Goddard <chase@chasegoddard.com>
2015-05-28 14:23:46 -0400

    sf

--- typeahead_implementation.js
+++ typeahead_implementation.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	var data = $("#class_data").html().split('\n');
 	data = $.grep(data, function(val){
 		if(val.indexOf("12") > -1){
-			console.log(val.split(","));
-			return val.split(",")[0] + val.split(",")[1];
+			return val;
 		}
+	});
+	data = $.map(data, function(val){
+		return val.split(",")[0] + val.split(",")[1];
 	});
 	console.log(data);
 	$("#name").typeahead({
