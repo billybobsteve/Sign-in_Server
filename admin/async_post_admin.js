64e4506a74173f497e2167ddf7a47c71e3529209
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-28 11:36:03 -0400

    testing

--- admin/async_post_admin.js
+++ admin/async_post_admin.js
@@ -54,6 +54,7 @@ var edit = function(obj){
 
 function return_data(data, textStatus, jqXHR) {
 	//if (location.pathname.includes("admin.php")) {
+		console.log(data);
 		$("#table").html(data);
 		$(".table-entry").append("<a href='#' class='edit-button'>Edit</a>");
 		$(".edit-button").click(function(){
