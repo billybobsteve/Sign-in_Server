b9c45eb99e61a9fbbc96e12ef890e00ddd8a8a68
Chase Goddard <chase@chasegoddard.com>
2015-05-28 14:05:43 -0400

    slfj

--- typeahead_implementation.js
+++ typeahead_implementation.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+	var data = $("#class_data").html().split('\n');
+	$.grep(data, function(val){
+		if(val.indexOf("12") > -1){
+			return val;
+		}
+	});
+	console.log(data);
 	$("#name").typeahead({
-		name: 'accounts',
+		name: 'names',
 		local: ['Chase Goddard', 'Thomas Ragucci', 'Nathan Spring', 'Harrison Lee', 'Some Random Bum']
 	})
 });
