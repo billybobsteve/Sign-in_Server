c57f9088d3b2cc168c1b60ad8b156b1f7ce78789
Idoncar <nathanspring0@gmail.com>
2015-05-28 14:01:29 -0400

    asdf

--- typeahead_implementation.js
+++ typeahead_implementation.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#name").typeahead({
 		name: 'accounts',
-		local: ['Chase Goddard', 'Thomas Ragucci', 'Nathan Spring', 'Harrison Lee', 'Some Random Bum']
+		local: ['Nicolas Wilmer', 'Nine Times Ten', 'Never Say Never', 'Now I go Home L0LL','Chase Goddard', 'Thomas Ragucci', 'Nathan Spring', 'Harrison Lee', 'Some Random Bum']
 	})
 });
