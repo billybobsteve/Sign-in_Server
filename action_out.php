a0351a644f81740306c26b69db70480ef13993ed
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-28 09:21:51 -0400

    more test

--- action_out.php
+++ action_out.php
@@ -32,9 +32,9 @@ mysql_select_db('signoutdb');
 
 for ($i = 0; $i < count($name_array); $i++) {
 	$current_name = $name_array[$i];
-	die();
 	$sql_name_check = "SELECT * FROM Students WHERE Name = '{$current_name}';";
 	$name_check = mysql_query($sql_name_check, $conn);
+	die();
 	if (mysql_num_rows($name_check) > 0) {
 		echo '-1' . ',';
 		continue;
