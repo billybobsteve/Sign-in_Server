c21c846f19f9fecd6ee467606507aef0094d22a0
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-29 09:12:17 -0400

    test

--- admin/action_admin.php
+++ admin/action_admin.php
@@ -107,7 +107,7 @@ switch ($option) {
 			
 		}
 		fclose($myfile);
-		echo "Database has been printed in database.txt: <form method='get' action='database.txt'> <button type='submit'>Download database</button> </form>";
+		echo "Database has been printed in database.txt: <a href='database.txt'>Download database</a> (right click: 'save link as' to download.)";
 		break;
 		
 }
