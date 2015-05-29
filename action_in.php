a3e4804d4f3d040a711b3f89700aaae1482ec3a1
SomeGuy4 <thomas_ragucci@ryecountryday.org>
2015-05-28 21:17:46 -0400

    sql

--- action_in.php
+++ action_in.php
@@ -1,9 +1,7 @@
 <?php 
 
 $name = htmlspecialchars($_POST['name']);
-$name = mysql_real_escape_string($name);
 $time = htmlspecialchars($_POST['datetime']);
-$time = mysql_real_escape_string($time);
 $current_date = new DateTime();
 $time = $current_date->format('Y-m-d ') . $time;
 
@@ -20,37 +18,41 @@ $name_array = explode(',', $name);
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'ec2inmybutt';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
 if (!$conn) {
-	die('Could not connect: ' . mysql_error());
+	die('Could not connect: ' . mysqli_error($conn));
 }
+
+$name = mysqli_real_escape_string($conn, $name);
+
+$time = mysqli_real_escape_string($conn, $time);
 
 //$sql = "";
 
 //echo $sql;
 
-mysql_select_db('signoutdb');
+mysqli_select_db($conn, 'signoutdb');
 
 for ($i = 0; $i < count($name_array); $i++) {
 	$current_name = $name_array[$i];
 	$sql_name_check = "SELECT * FROM Students WHERE Name = '{$current_name}' AND TimeIn IS NULL;";
 	$name_check = mysql_query($sql_name_check, $conn);
-	if (mysql_num_rows($name_check) == 0) {
+	if (mysqli_num_rows($conn, $name_check) == 0) {
 		echo '-1' . ',';
 		continue;
 	}
 	$sql = "UPDATE Students SET TimeIn='{$time_in_str}', ServerTimeIn='{$server_time_in}' WHERE name='{$current_name}';";
-	$retval = mysql_query($sql, $conn);
+	$retval = mysqli_query($conn, $sql);
 
 	if(! $retval ) {
-	  die('Could not enter data: ' . mysql_error());
+	  die('Could not enter data: ' . mysqli_error($conn));
 	}
 	//echo $current_name . ','; //"\r\n"; 
 	echo '1' . ',';
 }
 
-mysql_close($conn);
+mysqli_close($conn);
 
 //echo "{$name} has just been signed out to {$dest}"; 
 
