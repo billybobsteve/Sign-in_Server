b3735ce2e687e46bc89ed60452edf57d2017fb01
Idoncar <nathanspring0@gmail.com>
2015-05-28 23:44:32 -0400

    fffff

--- script-front.js
+++ script-front.js
@@ -163,7 +163,7 @@ $(document).ready(function(){
 	$("#out-tab").click(function(){
 		$("#out-tab").addClass("activeLink");
 		$("#in-tab").removeClass("activeLink");
-		$("#destination").removeClass("hide");
+		$("#dest-div").removeClass("hide");
 		$("#sign-in-instructions").addClass("hide");
 		$("#sign-out-instructions").removeClass("hide");
 		$("#time-label").html("Time Out: ");
