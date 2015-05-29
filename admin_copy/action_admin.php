d8a2dc0020b74bde4a83ac19b30b0bf52cbe4866
Idoncar <nathanspring0@gmail.com>
2015-05-28 21:24:21 -0400

    adsfij

--- admin_copy/action_admin.php
+++ admin_copy/action_admin.php
@@ -16,14 +16,14 @@ mysql_select_db('signoutdb');
 switch ($option) {
 	case "out_students" :
 		//echo "<h1>Students</h1>";
-		echo "<div style='display:inline-flex;'>";
+		echo "<div id = "tablediv" style='display:inline-flex;'>";
 		#echo "Name: <input class='text-box' id='name' style='width:150px' type='text'>";
 		#echo "Location: <input class='text-box' id='dest' style='width:150px' type='text'>";
 		#echo "Time Out: <input class='text-box' id='time_out' style='width:150px' type='text'>";
 		#echo "Server Time Out: <input class='text-box' id='server_time_out' style='width:150px' type='text'>";
 		#echo "Time In: <input class='text-box' id='time_in' style='width:150px' type='text'>";
 		#echo "Server Time In: <input class='text-box' id='server_time_in' style='width:150px' type='text'> <input class='text-box' id='server_time_in' style='width:150px' type='text'> </div>";
-		echo "<table border = '1px'>";
+		echo "<table id = "table" border = '1px'>";
 		echo "<tr> <th>Id</th> <th>Name</th> <th>Location</th> <th>Time Out</th> <th>Server Time Out</th> <th>Time In</th> <th>Server Time In</th></tr>";
 		//echo "out_students";
 		$sql = 'SELECT * FROM Students WHERE TimeIn IS NULL;';
